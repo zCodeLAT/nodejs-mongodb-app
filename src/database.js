4f890f5340baad0b4f4b53c182f16ba346c68f21
zCodeLAT <esteban.martin7@gmail.com>
2021-10-24 23:01:30 -0300

    db ATLAS test

--- src/database.js
+++ src/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/db_images', {
+mongoose.connect('mongodb+srv://dbUser:AZSXDC$13579@cluster0.e5vyu.mongodb.net/Cluster0?retryWrites=true&w=majority', {
     useNewUrlParser: true
 })
     .then(db=>console.log('DB is connected'))
